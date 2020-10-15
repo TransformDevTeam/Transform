c4e4b05da660145ed6f2e8703c1049b2e11e7d9d
Ilan Harari <ilanhrr99@gmail.com>
2020-10-14 17:11:14 -0700

    Initial Commit

---
+++ Transform/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Transform
+//
+//  Created by Ilan Harari on 10/14/20.
+//  Copyright © 2020 Ilan Harari. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
